2d2b780d19540a4a3039f49c2392a6b6c97a0db0
EC2 Default User <ec2-user@ip-172-31-19-56.ap-southeast-2.compute.internal>
2023-10-13 08:40:01 +0000

    RSpec Test

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,10 @@
 class BooksController < ApplicationController
-  def new
-  end
 
   def create
     @book = Book.new(book_params)
     if @book.save
       flash[:notice] = "Book was successfully created."
-      redirect_to book_path(@book.id)
+      redirect_to book_path(@book)
     else
       @books = Book.all
       render :index
@@ -30,7 +28,7 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     if @book.update(book_params)
       flash[:notice] = "Book was successfully updated."
-      redirect_to book_path(@book.id)
+      redirect_to book_path(@book)
     else
       render :edit
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get '/' => 'homes#top'
+  root to: 'homes#top'
   resources :books
-  get 'books' => 'books#index' 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
